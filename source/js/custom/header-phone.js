a50fc73773bb2dbea0299f96b0c09fcdbdb8a3f5
Mirek Benes <mirek.benes@gmail.com>
2022-09-15 14:37:34 +0200

    odstrani scriptu pro telefon v hlavicce

--- source/js/custom/header-phone.js
+++
@@ -1,9 +0,0 @@
-// Header Phone
-function headerPhone() {
-    const phoneContainer = document.querySelector(".top-info"),
-        phoneText = document.querySelector(".top-info").innerHTML,
-        beforePhone = phoneText.split('+')[0],
-        phoneNumber = phoneText.substring(phoneText.indexOf('+') - 1);
-
-    phoneContainer.innerHTML = beforePhone + '<a href="tel:'+phoneNumber+'">'+phoneNumber+'</a>';
-}
